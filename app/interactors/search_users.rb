7ab238ed973357b14ca2b8fcf6f3d81bdf8e96df
waleedamjad13 <waleedamjad57@gmail.com>
2023-05-15 23:28:53 +0500

    add search to users table

--- app/interactors/search_users.rb
+++ app/interactors/search_users.rb
@@ -4,8 +4,7 @@ class SearchUsers < ApplicationInteractor
     search_terms = context.search_terms.split(" ")
 
     users = User.all
-    users = users.where("firstname LIKE ? OR lastname LIKE ?",
-                        "%#{search_terms.first}%", "%#{search_terms.last}%")
+    users = users.where("firstname LIKE :term OR lastname LIKE :term OR email LIKE :term", term: "%#{search_terms.first}%")
 
     context.users = users
   end
