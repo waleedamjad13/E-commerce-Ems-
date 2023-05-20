53c11871694d4a755a3bdfa6a382cd1c9fd81099
waleedamjad13 <waleedamjad57@gmail.com>
2023-05-21 00:09:30 +0500

    feedback of PR

--- app/view_models/product_view.rb
+++ app/view_models/product_view.rb
@@ -8,9 +8,12 @@ class ProductView < ApplicationView
   delegate :title, :description, :price, :status, :header_image, :category_id, :images,
     to: :product
 
-
+  def status_options
+    Product.statuses.keys.map { |s| [s.titleize, s] }
+  end
+  
   def category_options
-    Category.all.map { |category| [category.id, category.name]}
+    Category.all.map { |category| [category.name, category.id]}
   end
 
 end
